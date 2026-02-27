d884955c5cab65de0b9d0c9ce96c6cb440c078d8
Amadou-Cly <amadou.coulibaly@venta.lv>
2026-02-27 15:42:59 +0200

    Initl: new seminar for java varibnales anf getters

--- src/model/Student.java
+++ src/model/Student.java
@@ -1,5 +1,40 @@
 package model;
 
 public class Student {
+	private String matriculaNumber;
+	private String name;
+	private String surname;
+	private String faculty;
+	private int birthYear;
+	private String passportNumber;
+	
+	//2. getters
+	
+	public String getMatricularNumber() {
+		return matriculaNumber;
+	}
+	
+	public String getName() {
+		return name;
+	}
 
+	public String getSurname() {
+		return  surname;
+	}
+	public String getFaculty() {
+		return faculty;
+	}
+	public int getBirthYear() {
+		return birthYear;
+	}
+	public String getPassportNumber() {
+		return passportNumber;
+	}
+	//Setters 
+	
+	public void setName(String inputName) {
+		if((inputName != null)&&(!inputName.isEmpty()) && (inputName.matches("")))
+		name = inputName;
+	}
+	
 }
