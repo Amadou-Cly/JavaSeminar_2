b233cd4cc6f96c7c10a4ba87636156b1e8b3601d
Amadou-Cly <amadou.coulibaly@venta.lv>
2026-02-27 16:10:45 +0200

    Setter

--- src/model/Student.java
+++ src/model/Student.java
@@ -1,5 +1,7 @@
 package model;
 
+import java.security.PublicKey;
+
 public class Student {
 	private String matriculaNumber;
 	private String name;
@@ -32,9 +34,15 @@ public class Student {
 	}
 	//Setters 
 	
+	public void setMatricularNUmber()
+	
 	public void setName(String inputName) {
-		if((inputName != null)&&(!inputName.isEmpty()) && (inputName.matches("")))
+		if((inputName != null)&&(!inputName.isEmpty()) && (inputName.matches("[A-Z]{1}[a-z]{15}[ ]{1}[A-Z]{1}[a-z]{15}"))) {
 		name = inputName;
+	}else {
+		name= "Unkwon";
 	}
+	
+	public void 
 	
 }
