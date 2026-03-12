cb14e1615f048ab90b1631d45016f158b4bdd82f
Amadou-Cly <amadou.coulibaly@venta.lv>
2026-03-12 09:49:45 +0200

    Setter

--- src/model/Student.java
+++ src/model/Student.java
@@ -9,6 +9,7 @@ public class Student {
 	private String faculty;
 	private int birthYear;
 	private String passportNumber;
+	private String country; 
 	
 	//2. getters
 	
@@ -32,14 +33,17 @@ public class Student {
 	public String getPassportNumber() {
 		return passportNumber;
 	}
+	public String getCountry() {
+		return country;
+	}
 	//Setters 
 	
 	public void setMatricularNUmber(String inputMatricularNumber) {
-		if((inputMatricularNumber != null)&&(!inputMatricularNumber.isEmpty()) && (inputMatricularNumber.matches("[A-Z]{1}[a-z]{15}[ ]{1}[A-Z]{1}[a-z]{15}")))
-	}
+		if((inputMatricularNumber != null)&&(!inputMatricularNumber.isEmpty()) && (inputMatricularNumber.matches("[A-Z]{1}[a-z]{15}[ ]{1}[A-Z]{1}[a-z]{15}"))) {
+	
 		matriculaNumber = inputMatricularNumber;
 		}
-	
+	}
 	
 	public void setName(String inputName) {
 		if((inputName != null)&&(!inputName.isEmpty()) && (inputName.matches("[A-Z]{1}[a-z]{15}[ ]{1}[A-Z]{1}[a-z]{15}"))) {
@@ -47,7 +51,32 @@ public class Student {
 	}else {
 		name= "Unkwon";
 	}
+	}
+	
+	public void setsurname(String inputSurname) {
+		if((inputSurname != null)&&(!inputSurname.isEmpty()) && (inputSurname.matches("[A-Z]{1}[a-z]{15}[ ]{1}[A-Z]{1}[a-z]{15}"))) {
+			surname = inputSurname;
+		}else {
+			surname= "Unkwon";
+			}
+			}
+	public void setFaculty(String inputFaculty) {
+		if((inputFaculty != null)&&(!inputFaculty.isEmpty()) && (inputFaculty.matches("[A-Z]{30}"))) {
+			faculty = inputFaculty;
+		}else {
+			faculty= "Unkwon";
+			}
+	}
+	public void setBirthYear(int inputBirthYear) {
+		if((inputBirthYear >= 1950) && (inputBirthYear <= 2020)) {
+			birthYear = inputBirthYear;
+		}else {
+			inputBirthYear = 2008;
+			}
+	}
+	public void setPassportNumber(inputPassportNumber){
+		
+	}
 	
 
 	
-}
